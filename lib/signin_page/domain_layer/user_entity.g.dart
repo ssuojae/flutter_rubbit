fd354d77957c0fff7017c45901239f04f385be37
suojae <suojae.developer@gmail.com>
2024-10-17 21:38:23 +0900

    feat: user entity 추가

---
+++ lib/signin_page/domain_layer/user_entity.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_entity.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$UserEntityImpl _$$UserEntityImplFromJson(Map<String, dynamic> json) =>
+    _$UserEntityImpl(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      email: json['email'] as String,
+    );
+
+Map<String, dynamic> _$$UserEntityImplToJson(_$UserEntityImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'email': instance.email,
+    };
